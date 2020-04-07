ca9ed354ce000d9eac6a5d0f017418664660620e
stelios.kitziris@partners-netguru.com <stelios.kitziris@partners-netguru.com>
2020-04-07 21:48:47 +0300

    Add valuidation Alerts and extra checks

--- api/app/Http/Controllers/CurrencyController.php
+++ api/app/Http/Controllers/CurrencyController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Currency;
 
 class CurrencyController extends Controller
 {
-    //
+    public function all() {
+        return Currency::all();
+    }
 }
